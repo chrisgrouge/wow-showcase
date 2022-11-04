20816486dd949a7fe75d6a36a3be718de9b0191d
Chris Grouge <cgrouge@whereoware.com>
2022-11-04 08:37:08 -0400

    adding this page until I figure out how to filter dynamically

---
+++ pages/email.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
+import Layout from '../components/shared/Layout';
+import PrimaryHeader from '../components/shared/PrimaryHeader';
+import ShowcaseList from '../components/domain/Home/ShowcaseList';
+import projects from '../data/projects';
+import pageTransitionVariants from '../utils/pageTransition';
+
+function Home() {
+  const router = useRouter();
+  console.log(router.pathname);
+  const { emails } = projects;
+
+  return (
+    <motion.div
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      className="p-home"
+      variants={pageTransitionVariants}
+    >
+      <Head>
+        <title>Showcase</title>
+      </Head>
+      <PrimaryHeader headline="Showcase" />
+      <ShowcaseList work={emails} />
+    </motion.div>
+  );
+}
+
+Home.layout = Layout;
+
+export default Home;
